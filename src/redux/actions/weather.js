c73066a8858b7202156c2cd1b5ab46ba8dd83070
Zidni Hidayat Edby <35618964+zidni0192@users.noreply.github.com>
2021-03-15 15:52:56 +0700

    Update changes

--- src/redux/actions/weather.js
+++ src/redux/actions/weather.js
@@ -3,7 +3,7 @@ export const getWeather = (prop) => {
   return {
     type: "GET_WEATHER",
     payload: axios.get(
-      ` https://cors-anywhere.herokuapp.com/http://samples.openweathermap.org/data/2.5/forecast/daily?q=Jakarta&appid=KeyName%2080b953d69225206b9ee463f4fda03391%20Default%20daa8e43240fb0cf6f10e5189eaa66012`
+      `http://samples.openweathermap.org/data/2.5/forecast/daily?q=Jakarta&appid=KeyName%2080b953d69225206b9ee463f4fda03391%20Default%20daa8e43240fb0cf6f10e5189eaa66012`
     ),
     meta:{city:prop}
   };
